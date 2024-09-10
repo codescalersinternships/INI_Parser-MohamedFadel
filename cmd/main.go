6fbd6a2ed7a3b301f1e5766b51f9aa5426b419ab
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-09-10 15:18:27 +0300

    fix(CI): fix linting and testing errors

--- cmd/main.go
+++ cmd/main.go
@@ -1,64 +1,61 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/codescalersinternships/INI_Parser-MohamedFadel/internal"
-)
-
 func main() {
-	parser := internal.INIParser{}
-	parser2 := internal.INIParser{}
+	/* I'm just testing the functionality
 
-	parser.LoadFromString(`; last modified 1 April 2001 by John Doe
-[owner]
-name = John Doe
-organization = Acme Widgets Inc.
 
-[database]
-# use IP address in case network name resolution is not working
-server = 192.0.2.62
-port = 143
-file = payroll.dat
-`)
+		parser := internal.INIParser{}
+		parser2 := internal.INIParser{}
 
-	parser2.LoadFromFile("../../../../INI.txt")
+		parser.LoadFromString(`; last modified 1 April 2001 by John Doe
+	[owner]
+	name = John Doe
+	organization = Acme Widgets Inc.
 
-	sections, err := parser.GetSectionNames()
-	if err != nil {
-		log.Fatal(err)
-	}
+	[database]
+	# use IP address in case network name resolution is not working
+	server = 192.0.2.62
+	port = 143
+	file = payroll.dat
+	`)
 
-	for _, section := range sections {
-		fmt.Println(section)
-	}
+		parser2.LoadFromFile("../../../../INI.txt")
 
-	data := parser2.GetSections()
-	for section, keyValue := range data {
-		fmt.Println("[", section, "]")
-
-		for key, value := range keyValue {
-			fmt.Println(key, "=", value)
+		sections, err := parser.GetSectionNames()
+		if err != nil {
+			log.Fatal(err)
 		}
-	}
 
-	parser.Set("owner", "name", "WalterWhite")
-	value, err := parser.Get("owner", "name")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(value)
+		for _, section := range sections {
+			fmt.Println(section)
+		}
 
-	stringData, err := parser.ToString()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(stringData)
+		data := parser2.GetSections()
+		for section, keyValue := range data {
+			fmt.Println("[", section, "]")
 
-	_, err = parser2.SaveToFile("../../../../newINI.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+			for key, value := range keyValue {
+				fmt.Println(key, "=", value)
+			}
+		}
+
+		parser.Set("owner", "name", "WalterWhite")
+		value, err := parser.Get("owner", "name")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(value)
+
+		stringData, err := parser.ToString()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(stringData)
+
+		_, err = parser2.SaveToFile("../../../../newINI.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
 
 }
